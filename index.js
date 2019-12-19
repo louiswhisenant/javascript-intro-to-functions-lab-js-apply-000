d12ee39107f4693af0f0f09455d955cd027ff1ed
Louis Whisenant <louiswhisenant@gmail.com>
2019-12-19 07:07:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-function shout(string) {
+function shout (string) {
   return string.toUpperCase();
 }
 
-function whisper(string) {
+function whisper (string) {
   return string.toLowerCase();
 }
 
+function logShout (string) {
+  console.log (string.toUpperCase());
+}
